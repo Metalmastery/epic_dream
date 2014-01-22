74a91aac58c91786bf554672c4569bc650ef989c
Metalmastery <metalmastery@gmail.com>
2014-01-22 02:55:03 +0200

    explosion particle system changed

--- js/objects/explosion.js
+++ js/objects/explosion.js
@@ -2,13 +2,16 @@ var Explosion = (function(){
     var pool = [],
         position = 0,
         poolSize = 100,
-        explosionSize = 100,
+        explosionSize = 50,
         pi2 = Math.PI * 2,
-        mainScene = null;
+        mainScene = null,
+        baseColor = 0xcc9944,
+//        baseColor = 0xff6644,
+        particleSize = 110;
 
     for (var i = 0; i < poolSize; i++) {
         var particles = new THREE.Geometry(),
-            material, explosion;
+            explosion, material;
 
         for (var j = 0; j<explosionSize; j++){
             var a1, a2, x, y, z;
@@ -21,23 +24,47 @@ var Explosion = (function(){
 
             particles.vertices.push(new THREE.Vector3(x,y,z));
         }
-        material = new THREE.ParticleSystemMaterial({size: 10, color: 0xff0000});
+
+        //        material = new THREE.ParticleSystemMaterial({size: 10, color: 0xff0000});
+        material = new THREE.ParticleBasicMaterial({
+            color: baseColor,
+            size: particleSize,
+            map: THREE.ImageUtils.loadTexture(
+                "img/explosion_particle.png"
+            ),
+            blending: THREE.AdditiveBlending,
+            depthWrite : false,
+            transparent: true
+        });
         explosion = new THREE.ParticleSystem(particles, material);
-        explosion.material.transparent = true;
+//        explosion.material.transparent = true;
+//        explosion.sortParticles = true;
         pool.push(explosion);
+    }
+
+    function resetItem(obj){
+        obj.material.opacity = 1;
+        obj.material.size = particleSize;
+        obj.material.color.setHex(baseColor);
+        obj.scale.setLength(3);
     }
 
     function run(scene){
         var item = pool[position], length = 10;
         scene.add(item);
         function callback(){
-            item.material.opacity -= 0.02;
+            item.material.opacity -= 0.05;
             if (item.material.opacity > 0) {
-                length+=5;
+                length *=1.12;
+                item.material.size -= 5;
+//                item.rotation.z += 0.05;
+//                item.material.color.offsetHSL(0,0.05,0);
+                item.material.color.offsetHSL(0,0.09,0);
                 item.scale.setLength(length);
                 requestAnimationFrame(callback);
             } else {
                 scene.remove(item);
+                resetItem(item);
             }
         }
         requestAnimationFrame(callback);
@@ -45,6 +72,7 @@ var Explosion = (function(){
 
     return {
         detonate : function(coord){
+//            console.log(pool);
             position++;
             if (position >= pool.length){
                 position = 0;
